8defdd321cc0b6108708ff6143050ee3b497677e
moquyun188 <116344547@qq.com>
2018-03-10 23:21:56 +0800

    web a

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 't',  'namespace' => 'Test'], function () {
+	
+	Route::get('a', [ 'as' => 'test.a', 'uses' => 'IndexController@hk_key']);
+	/*测试END*/
+	//Route::get('pub/login', [ 'as' => 'wxbot.login', 'uses' => 'PubController@Login']);
+	//Route::post('pub/login', [ 'as' => 'wxbot.login', 'uses' => 'PubController@Login']);
+	//Route::get('pub/moonshare', [ 'as' => 'wxbot.moonshare', 'uses' => 'PubController@Moon_shareinfo']);
+	//Route::post('pub/moonshare', [ 'as' => 'wxbot.moonshare', 'uses' => 'PubController@Moon_shareinfo']);
+	//Route::post('login', [ 'as' => 'admin.auth.login', 'uses' => 'AuthController@postLogin']);
+	//Route::post('logout', [ 'as' => 'admin.auth.logout', 'uses' => 'AuthController@postLogout']);
+
+	
+});
